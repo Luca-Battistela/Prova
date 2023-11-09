588bb79653f38101de0080c5767da3da6332678d
bttst <bttst@LAPTOP-9ETIBDCI.fritz.box>
2023-11-09 17:56:58 +0100

    Initial commit

--- src/Prova/Prova.java
+++ src/Prova/Prova.java
@@ -3,7 +3,10 @@ package Prova;
 public class Prova {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n = 5;
+		for(int i=0; i<5; i++) {
+			System.out.println("Prova");
+		}
 
 	}
 
